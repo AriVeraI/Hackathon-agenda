b170a62622385355447e0561f504cdfb05a11405
AriVeraI <verariadn@gmail.com>
2026-08-03 16:10:19 -0600

    Método Agenda llena

--- Hkn-agenda/src/ContactoService.java
+++ Hkn-agenda/src/ContactoService.java
@@ -4,8 +4,37 @@ import java.util.List;
 
 public class ContactoService {
     private List<Contacto> contactos;
+    // Capacidad máxima de la agenfa
+    private int limiteMaximo;
 
-    public ContactoService(List<Contacto> contactos) {
+//    public ContactoService(List<Contacto> contactos) {
+    public ContactoService(int limiteMaximo) {
         this.contactos = new ArrayList<>();
+        // Constructor con tamaño personalizado:
+        this.limiteMaximo = limiteMaximo;
+    }
+    // Constructor con capacidad de 10 contactos)
+    public ContactoService () {
+        this (10);
+    }
+
+    //Métodos:
+
+
+    //6. agendaLlena(), regresa "true" si la agenda está llena, y "false" en caso contrario
+    public boolean agendaLlena () {
+        // contactos.size() nos va a dar el número de los contactos existentes
+        if (this.contactos.size() >= this.limiteMaximo) {
+            System.out.println("La agenda está llena. No hay más espacio disponible para nuevos contactos. Lo lamento mucho :/");
+            return true;
+        } else {
+            System.out.println("La agenda aún tiene espacio disponible, puedes continuar agregando amigos C:");
+            return false;
+        }
+    }
+    // Método extra para probar
+    public List<Contacto> getContactos() {
+        return contactos;
     }
 }
+
--- Hkn-agenda/src/Main.java
+++ Hkn-agenda/src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
+        //Agenda de prueba
+        ContactoService agendaPrueba = new ContactoService(2);
+        Contacto c1 = new Contacto("Mariana", 55123458, Tipo.PERSONAL);
+        Contacto c2 = new Contacto("Dante", 55345670, Tipo.EMPRESA);
+
+        // Prueba 1 si esta llena al crearse, debe dar "false"
+        System.out.println("Prueba 1");
+        agendaPrueba.agendaLlena();
+        agendaPrueba.getContactos().add(c1); //agregar manialmente los dos contactos suponiendo que hay un getter o un método para añadirlos
+        agendaPrueba.getContactos().add(c2);
+        // Prueba 2, si es que esta llena
+        System.out.println("\n Prueba 2");
+        agendaPrueba.agendaLlena();
+
 
     }
 }
