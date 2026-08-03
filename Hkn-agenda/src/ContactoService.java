73ef96b2733d6cf77fef178ad7c0e9829288c34c
Miguel Flores <floresjosex50@gmail.com>
2026-08-03 13:46:18 -0600

    Agregar buscar contacto

--- Hkn-agenda/src/ContactoService.java
+++ Hkn-agenda/src/ContactoService.java
@@ -1,11 +1,29 @@
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 public class ContactoService {
     private List<Contacto> contactos;
 
-    public ContactoService(List<Contacto> contactos) {
+    public ContactoService() {
         this.contactos = new ArrayList<>();
+    }
+
+    public Optional<Contacto> existeContactoPorTelefono(Integer numero){
+        if (numero == null) return Optional.empty();
+
+        return this.contactos.stream()
+                .filter(contacto -> Objects.equals(contacto.getNumero(), numero))
+                .findFirst();
+    }
+
+    public Optional<Contacto> existeContactoPorNombre(String nombre){
+        if (nombre == null) return Optional.empty();
+
+        return this.contactos.stream()
+                .filter(contacto -> contacto.getNombre().equalsIgnoreCase(nombre))
+                .findFirst();
     }
 }
